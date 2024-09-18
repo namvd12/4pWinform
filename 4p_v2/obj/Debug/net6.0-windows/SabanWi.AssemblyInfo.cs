9ac10ed1749e67e7fe57ce496df821278313738e
vu dinh nam <vudinhnambk01@gmail.com>
2024-09-18 08:51:57 +0700

    commit 18/9/2024

--- 4p_v2/obj/Debug/net6.0-windows/SabanWi.AssemblyInfo.cs
+++ 4p_v2/obj/Debug/net6.0-windows/SabanWi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SabanWi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d96d79b3a2b65ae5fb08a086a8b1cabc2579ffe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea5799c1b931d1627e1ac50d2d5dd9dc6727f8dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SabanWi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SabanWi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
